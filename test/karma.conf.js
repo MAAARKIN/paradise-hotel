cc6cf753948a57d0e620d07e7ae4256d8b98263e
Marcos Filho <m.filhow@gmail.com>
2016-02-10 23:36:49 -0300

    test deploy

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,6 +33,11 @@ module.exports = function(config) {
       'bower_components/angular-moment/angular-moment.js',
       'bower_components/angular-local-storage/dist/angular-local-storage.js',
       'bower_components/angular-jwt/dist/angular-jwt.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
+      'bower_components/jquery-ui/jquery-ui.js',
+      'bower_components/lodash/lodash.js',
+      'bower_components/angular-simple-logger/dist/angular-simple-logger.js',
+      'bower_components/angular-google-maps/dist/angular-google-maps.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
